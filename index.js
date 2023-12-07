6a0270ff36f3d927ba5fb3f249731820c28247df
eti kona <etikonapal@gmail.com>
2023-12-07 15:12:02 +0600

    app running on port successfully

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
@@ -18,3 +21,22 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Set view engine
+app.set("view engine", "ejs");
+
+// Set static folder
+app.set(express.static(path.join(__dirname, "public")));
+
+// Parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// Routing setup
+
+// Error Handling
+
+// Listening port
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on ${process.env.PORT}`);
+});
