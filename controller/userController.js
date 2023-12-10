3e7c4f46863800bfc557a1169579a34b895d2516
eti kona <etikonapal@gmail.com>
2023-12-10 21:58:35 +0600

    I check login , required login added

--- controller/userController.js
+++ controller/userController.js
@@ -1,80 +1,45 @@
-// External Import
-const bcrypt = require("bcrypt");
-const { unlink } = require("fs");
-const path = require("path");
-// Get Login Page
-async function getUsers(req, res, next) {
-  try {
-    const users = await users.find();
-    res.render("users", {
-      users: "users",
-    });
-  } catch (err) {
-    next(err);
-  }
-}
+// external imports
+const express = require("express");
+const { check } = require("express-validator");
 
-// add user
-async function addUser(req, res, next) {
-  let newUser;
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  if (req.files && req.files.length > 0) {
-    newUser = new User({
-      ...req.body,
-      avatar: req.files[0].filename,
-      password: hashedPassword,
-    });
-  } else {
-    newUser = new User({
-      ...req.body,
-      password: hashedPassword,
-    });
-  }
-  // Save User
-  try {
-    const result = await newUser.save();
-    res.status(200).json({
-      message: "User was added successfully",
-    });
-  } catch (err) {
-    res.status(500).json({
-      errors: {
-        common: {
-          message: "Unknown error occurred",
-        },
-      },
-    });
-  }
-}
-// remove user
-// async function removeUser(req, res, next) {
-//   try {
-//     const user = await user.findByIdAndDelete({
-//       _id: req.params.id,
-//     });
-//     // Remove user avatar
-//     if (user.avatar) {
-//       unlink(path.join(__dirname, `../public/uploads/avatars/${user.avatar}`));
-//     }
-
-//     res.status(200).json({
-//       message: "User was removed successfully",
-//     });
-//   } catch (err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     res.status(500).json({
-//       errors: {
-//         common: {
-//           msg: "Could not delete the user",
-//         },
-//       },
-//     });
-//   }
-// }
-module.exports = {
+// internal imports
+const {
   getUsers,
   addUser,
   removeUser,
-};
+} = require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
+
+const { checkLogin, requireRole } = require("../middlewares/common/checkLogin");
+
+const router = express.Router();
+
+// users page
+router.get(
+  "/",
+  decorateHtmlResponse("Users"),
+  checkLogin,
+  requireRole(["admin"]),
+  getUsers
+);
+
+// add user
+router.post(
+  "/",
+  checkLogin,
+  requireRole(["admin"]),
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
+
+// remove user
+router.delete("/:id", checkLogin, requireRole(["admin"]), removeUser);
+
+module.exports = router;
